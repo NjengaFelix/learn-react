721ef3b115b5d0ee15aec1bb5e3279352201cc63
felix <felixnjunge@rocketmail.com>
2022-08-16 15:08:49 +0300

    declarative & imperative

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-//React Global variable
-//Render(What to render,Where to render it)
-// ReactDOM.render(<h1>Hello Everyone</h1>,document.getElementById("root"))
+/*
+Imparative
+How should it be done?
+Describes every step on how something should be done 
+Vanilla JS is imparative (i.e.,)
+const h1 = document.createElement(h1);
+h1.className = "header";
+h1.textContent = "Vanilla JS is imparative";
+document.getElementById("root").append(h1);
+console.log(h1);
+*/
+/*
+Declarative
+What should be done?
+React simply requires the programmer to define what should be done
+as shown below
+*/
 
-//QuerySelector includes # for the id
-// ReactDOM.render(<h1>New chapter</h1>,document.querySelector("#root"))
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-md">
+            <a className="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+    )
+}
 
+function MainContent() {
+    return (
+        <h1>I'm Learning React</h1>
+    ) 
+}
+//Adding two reactComponents to the root div
 ReactDOM.render(
-    <ul>
-        <li>Ford</li>
-        <li>Pagani</li>
-    </ul>,
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
     document.getElementById("root")
 )
