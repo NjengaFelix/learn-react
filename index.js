0e2e7581115ce3916c22c91377de8bc0def1c570
felix <felixnjunge@rocketmail.com>
2022-08-17 13:55:46 +0300

    JSX

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-/*
-Imparative
-How should it be done?
-Describes every step on how something should be done 
-Vanilla JS is imparative (i.e.,)
-const h1 = document.createElement(h1);
-h1.className = "header";
-h1.textContent = "Vanilla JS is imparative";
-document.getElementById("root").append(h1);
-console.log(h1);
-*/
-/*
-Declarative
-What should be done?
-React simply requires the programmer to define what should be done
-as shown below
-*/
+//Javascript XML which was created by React
+//HTML in Javascript
 
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-md">
-            <a className="navbar-brand" href="#">Navbar</a>
-            </div>
-        </nav>
-    )
-}
-
-function MainContent() {
-    return (
-        <h1>I'm Learning React</h1>
-    ) 
-}
-//Adding two reactComponents to the root div
-ReactDOM.render(
-    <div>
-        <Navbar />
-        <MainContent />
-    </div>,
-    document.getElementById("root")
+const navbar = (
+    <nav>
+        <h1>My Brand</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
+
+ReactDOM.render(navbar,document.getElementById("root"))
